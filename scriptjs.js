7f9bc4a4a79770a83b85b73c67df0b1223329005
ywhach <146796559+ywhach@users.noreply.github.com>
2023-12-04 13:52:17 +0700

    Add files via upload

---
+++ scriptjs.js
@@ -0,0 +1,20 @@
+const wrapper = document.querySelector(".wrapper");
+const question = document.querySelector(".question");
+const gif = document.querySelector(".gif");
+const yesBtn = document.querySelector(".yes-btn");
+const noBtn = document.querySelector(".no-btn");
+yesBtn.addEventListener("click", () => {
+question.innerHTML = "Aaaaa, I like you too";
+gif.src =
+"https://discord.com/channels/1158324509724655729/1158324510483812375/1181124074521501706"; });
+
+noBtn.addEventListener("mouseover", () => {
+const noBtnRect = noBtn.getBoundingClientRect(); 
+const maxX = window.innerWidth - noBtnRect.width;
+const maxY = window.innerHeight - noBtnRect.height;
+
+const randomX = Math.floor(Math.random() * maxX);
+const randomY = Math.floor(Math.random() * maxY);
+noBtn.style.left = randomX + "px";
+noBtn.style.top = randomY + "px";
+});
